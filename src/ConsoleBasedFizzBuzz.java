e77224ec9125d9b6147137455f867153c5aae3be
LEON CRUZ JOSE EMILIO <zS23017360@estudiantes.uv.mx>
2025-03-04 09:59:50 -0600

    first commit

--- src/ConsoleBasedFizzBuzz.java
+++ src/ConsoleBasedFizzBuzz.java
@@ -7,11 +7,11 @@ public class ConsoleBasedFizzBuzz implements FizzBuzz {
     public void print(int from, int to) {
     while (to >= from) {
         if (from % 15 == 0) {
-            System.out.println("BuzzFizz" + " " + from);
+            System.out.println("FizzBuzz" + " " + from);
         } else if (from % 3  == 0 ) {
-            System.out.println("Buzz" + " " + from);
+            System.out.println("Fizz" + " " + from);
         } else if (from % 5== 0) {
-            System.out.println("Fizz" + " "+ from);
+            System.out.println("Buzz" + " "+ from);
         }
         from++;
         }
